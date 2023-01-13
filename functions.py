286fe0ddd86fba7393da88a7196b0c293f28fdee
Ayush Pawar <ayushmpawar@outlook.com>
2023-01-13 04:53:44 -0500

    added lesser of two evens

--- functions.py
+++ functions.py
@@ -31,4 +31,17 @@ def skyline(word):
     return skylined_word
 
 
-print(skyline('Anthropomorphism'))
+def lesser_of_two_evens(a, b):
+    if a % 2 == 0 and b % 2 == 0:
+        if a <= b:
+            return a
+        else:
+            return b
+    else:
+        if a >= b:
+            return a
+        else:
+            return b
+
+
+print(lesser_of_two_evens(2, 5))
